ebd1bc9fc5062a585bd2051bf3fab3b119b5b98b
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-02-24 18:51:46 +0800

    update clock display

--- app/clock.js
+++ app/clock.js
@@ -1,7 +1,6 @@
 import { me } from "appbit";
 import clock from "clock";
 import document from "document";
-import { preferences } from "user-settings"; 
 import * as util from "./utils.js";
 
 // TIME
@@ -13,6 +12,7 @@ let mins2 = document.getElementById("mins2");
 // Set clock granularity to seconds for real-time updates
 clock.granularity = "seconds";
 
+// Function to initialize the clock
 export function initializeClock() {
     clock.ontick = evt => {
         let d = evt.date;
@@ -22,28 +22,35 @@ export function initializeClock() {
         if (preferences.clockDisplay === "12h") {
             // 12h format
             hours = hours % 12 || 12;
+        } else {
+            // 24h format
+            hours = util.zeroPad(hours);
         }
         setHours(hours);
 
         // MINUTES
-        let minutes = d.getMinutes();
-        setMins(minutes);
-    }
+        let minute = ("0" + d.getMinutes()).slice(-2);
+        setMins(minute);
+    };
 }
 
 // Set hours display
 function setHours(val) {
-    drawDigit(Math.floor(val / 10), hours1); // Tens place
-    drawDigit(val % 10, hours2);             // Ones place
+    if (val > 9) {
+        drawDigit(Math.floor(val / 10), hours1);
+    } else {
+        drawDigit("", hours1);
+    }
+    drawDigit(Math.floor(val % 10), hours2);
 }
 
 // Set minutes display
 function setMins(val) {
-    drawDigit(Math.floor(val / 10), mins1); // Tens place
-    drawDigit(val % 10, mins2);             // Ones place
+    drawDigit(Math.floor(val / 10), mins1);
+    drawDigit(Math.floor(val % 10), mins2);
 }
 
 // Draw a digit on the display
 function drawDigit(val, place) {
-    place.image = `${val}.png`; // Set the image to the corresponding digit PNG
+    place.image = `${val}.png`;
 }
