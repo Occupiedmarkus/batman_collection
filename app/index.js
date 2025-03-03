de654fd94b29c50371f8b0fa1ecd7c0d59ef4a22
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-03 21:51:12 +0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import * as document from "document";
 import { initializeClock } from "./clock.js";  
 import { initializeDate } from "./date.js"; 
-import { initializeHR }from "./hrm.js";
+import { initializeHR } from "./hrm.js";
 import { updateBatteryLevel } from "./battery.js"; 
 import { peerSocket } from "messaging";
 import { updateWeatherDisplay } from "./weatherDisplay.js"; // Import the function
+import { checkWristStatus } from "./accelerometer.js";
 
 // Listen for incoming messages from the companion
 peerSocket.onmessage = (evt) => {
@@ -12,11 +13,13 @@ peerSocket.onmessage = (evt) => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+    checkWristStatus();
+  
     // Initialize the clock and date displays
     initializeClock(); // Initialize the clock display
     initializeDate(); // Initialize the date display
-    intervalFunction(); // Initialize heart rate monitoring
     initializeHR();
+
     // Update battery level display initially and then every minute
     updateBatteryLevel(); // Log the initial battery level
 });
