219a6e2ba55f55d39e3965624f0e71595718a4a9
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-03 21:53:31 +0800

    Update weatherDisplay.js

--- app/weatherDisplay.js
+++ app/weatherDisplay.js
@@ -1,40 +1,44 @@
-// app/weatherDisplay.js
 import document from "document";
 import { peerSocket } from "messaging";
 
 const weatherImage = document.getElementById("weatherImage");
 
-export function updateWeatherDisplay(evt) {
-    const { temperature, condition, locationName } = evt.data;
+export function updateWeatherDisplay(weatherUpdate) {
+    const { temp, cond, loc, uni } = weatherUpdate;
 
-    switch (condition) {
+    const weatherImage = document.getElementById("weatherImage");
+    console.log(`Weather: ${temp}°${uni} in ${loc} - Condition: ${cond}.`);
+
+    // Set the appropriate weather image based on the condition
+    switch (cond) {
         case "Cloudy":
         case "Overcast":
-            weatherImage.href = "weather/cloudy.png";
+            weatherImage.href = "../resources/weather/cloudy.png";
             break;
         case "Hot":
-            weatherImage.href = "weather/hot.png";
+            weatherImage.href = "../resources/weather/hot.png";
             break;
         case "HazySunshineDay":
         case "HazyMoonlight":
         case "PartlySunnyDay":
-            weatherImage.href = "weather/hazy_sunshine_day.png";
+            weatherImage.href = "../resources/weather/hazy_sunshine_day.png";
             break;
         case "SunnyDay":
-            weatherImage.href = "weather/sunny_day.png";
+            weatherImage.href = "../resources/weather/sunny_day.png";
             break;
         case "Rain":
         case "Showers":
         case "FreezingRain":
-            weatherImage.href = "weather/rain.png";
+            weatherImage.href = "../resources/weather/rain.png";
             break;
         case "Windy":
-            weatherImage.href = "weather/windy.png";
+            weatherImage.href = "../resources/weather/windy.png";
             break;
         case "Thunderstorms":
-            weatherImage.href = "weather/thunderstorms.png";
+            weatherImage.href = "../resources/weather/thunderstorms.png";
             break;
         default:
-            weatherImage.href = "weather/default.png"; // Clear image for unrecognized conditions
+            weatherImage.href = "../resources/weather/default.png"; // Clear image for unrecognized conditions
+            console.log("Default case triggered");
     }
 }
