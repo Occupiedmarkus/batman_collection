fd5cc21e71633dbda98e76e77be92e728a00d030
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-04 10:55:52 +0800

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -4,6 +4,7 @@ import { displayWeather } from "./weather.js"; // Importing from weather.js
 
 // Check for location permissions and display weather
 if (companion.permissions.granted("access_location")) {
+    console.log("Location permission granted.");
     displayWeather(); // Call the displayWeather function
 } else {
     console.error("Location permission not granted.");
