0b61b9dc535a2aad74322723f2c208e7ccd5a92c
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-05 16:37:58 +0800

    remove settings

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,16 +1,7 @@
 registerSettingsPage(({ settings }) => (
   <Page>
-    <Section
-      title={
-        <Text bold align="center">
-          Clockface Settings
-        </Text>
-      }
-    >
-      <Toggle
-        label="Enable background animation"
-        settingsKey="toggleAnimationInput"
-      />
+    <Section>
+    
     </Section>
   </Page>
 ));
