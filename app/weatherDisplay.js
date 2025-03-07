885e9f6dd4a34f90d5cf69e37bacf1ba54a3ad12
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-07 13:56:06 +0800

    Update weatherDisplay.js
    updated lowercase

--- app/weatherDisplay.js
+++ app/weatherDisplay.js
@@ -9,36 +9,48 @@ export function updateWeatherDisplay(data) {
 
     console.log(`Weather: ${temp}°${uni} in ${loc}-${cond}.`);
 
-    // Set the appropriate weather image based on the condition
-    switch (cond) {
-        case "Cloudy":
-        case "Overcast":
-            weatherImage.href = "../resources/weather/cloudy.png";
-            break;
-        case "Hot":
-            weatherImage.href = "../resources/weather/hot.png";
-            break;
-        case "HazySunshineDay":
-        case "HazyMoonlight":
-        case "PartlySunnyDay":
-            weatherImage.href = "../resources/weather/hazy_sunshine_day.png";
-            break;
-        case "SunnyDay":
-            weatherImage.href = "../resources/weather/sunny_day.png";
-            break;
-        case "Rain":
-        case "Showers":
-        case "FreezingRain":
-            weatherImage.href = "../resources/weather/rain.png";
-            break;
-        case "Windy":
-            weatherImage.href = "../resources/weather/windy.png";
-            break;
-        case "Thunderstorms":
-            weatherImage.href = "../resources/weather/thunderstorms.png";
-            break;
-        default:
-            weatherImage.href = "../resources/weather/default.png";
-            console.log("Default case triggered");
-    }
+// Set the appropriate weather image based on the condition
+switch (cond) {
+    case "Cloudy":
+    case "cloudy":
+    case "Overcast":
+    case "overcast":
+        weatherImage.href = "../resources/weather/cloudy.png";
+        break;
+    case "Hot":
+    case "hot":
+        weatherImage.href = "../resources/weather/hot.png";
+        break;
+    case "HazySunshineDay":
+    case "hazysunshineday":
+    case "HazyMoonlight":
+    case "hazymoonlight":
+    case "PartlySunnyDay":
+    case "partlysunnyday":
+        weatherImage.href = "../resources/weather/hazy_sunshine_day.png";
+        break;
+    case "SunnyDay":
+    case "sunnyday":
+        weatherImage.href = "../resources/weather/sunny_day.png";
+        break;
+    case "Rain":
+    case "rain":
+    case "Showers":
+    case "showers":
+    case "FreezingRain":
+    case "freezingrain":
+        weatherImage.href = "../resources/weather/rain.png";
+        break;
+    case "Windy":
+    case "windy":
+        weatherImage.href = "../resources/weather/windy.png";
+        break;
+    case "Thunderstorms":
+    case "thunderstorms":
+        weatherImage.href = "../resources/weather/thunderstorms.png";
+        break;
+    default:
+        weatherImage.href = "../resources/weather/default.png";
+        console.log("Default case triggered");
+}
 }
