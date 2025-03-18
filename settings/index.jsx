5566f0b1ec7a1a32c5bd8686f7f77db56309f290
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-18 15:34:20 +0800

    Finalised

--- settings/index.jsx
+++ settings/index.jsx
@@ -2,25 +2,22 @@ function Colors(props) {
   return (
     <Page>
       <Section
-        title={<Text bold align="center">Power Color</Text>}>
+        title={<Text bold align="center">Power Ring Color</Text>}>
         <ColorSelect
           settingsKey="myColor"
           colors={[
             {color: '#fe0000', name: 'Beyond Red'},
-            {color: 'crimson'},
-            {color: 'orange'},
-            {color: 'yellow'},
-            {color: 'limegreen'},
-            {color: 'dodgerblue'},
-            {color: 'indigo'},
-            {color: 'hotpink'},
-            {color: 'honeydew'},
-            {color: 'black'}
+            {color: 'crimson', name: 'Rage'},
+            {color: 'orange', name: 'Greed'},
+            {color: 'yellow', name: 'Fear'},
+            {color: 'limegreen', name: 'Will'},
+            {color: 'dodgerblue', name: 'Hope'},
+            {color: 'indigo', name: 'Compassion'},
+            {color: 'hotpink', name: 'Love'},
+            {color: 'honeydew', name: 'Life'},
+            {color: 'black', name: 'Deathbi'},
           ]}
         />
-        <Toggle>
-
-        </Toggle>
       </Section>
     </Page>
   );
