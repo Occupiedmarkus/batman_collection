76a2aeb112591e95d54b2cd00dd85f416ac98f45
Occupiedmarkus <100002192+Occupiedmarkus@users.noreply.github.com>
2025-03-20 10:54:47 +0800

    Update Flashing
    -color filtering tint will now animate along with bat logo
    -Viibrant color options

--- settings/index.jsx
+++ settings/index.jsx
@@ -6,16 +6,16 @@ function Colors(props) {
         <ColorSelect
           settingsKey="myColor"
           colors={[
-            {color: '#fe0000', name: 'Beyond Red'},
-            {color: 'crimson', name: 'Rage'},
-            {color: 'orange', name: 'Greed'},
-            {color: 'yellow', name: 'Fear'},
-            {color: 'limegreen', name: 'Will'},
-            {color: 'dodgerblue', name: 'Hope'},
+            {color: '#ff0000', name: 'Beyond Red'},
+            {color: '#ff1744', name: 'Rage'},
+            {color: '#ff6d00', name: 'Greed'},
+            {color: '#ffd600', name: 'Fear'},
+            {color: '#00ff00', name: 'Will'},
+            {color: '#00e5ff', name: 'Hope'},
             {color: 'indigo', name: 'Compassion'},
-            {color: 'hotpink', name: 'Love'},
+            {color: '#ff00ff', name: 'Love'},
             {color: 'honeydew', name: 'Life'},
-            {color: 'black', name: 'Deathbi'},
+            {color: 'black', name: 'Death'},
           ]}
         />
       </Section>
